a4180323a33bc35c5b402d1fee663499066194db
pDiazz-dev <pedrodias20152015@gmail.com>
2025-09-24 15:33:06 -0300

    Inicio da tela de Cadastro, mudando apenas o visual do formulario

--- Login/Cadastro/ScriptCadastro.js
+++ Login/Cadastro/ScriptCadastro.js
@@ -3,10 +3,10 @@ const confirmar = document.getElementById("confirmar")
 
 
 confirmar.addEventListener("click", (()=> {
-    const password = document.getElementById("PasswordRegister").value
+    const password = document.getElementById("Password").value
     const password2 = document.getElementById("confirmPassword").value
-    const email = document.getElementById("EmailRegister").value
-    const usuario = document.getElementById("UsuarioRegister").value
+    const email = document.getElementById("Email").value
+    const usuario = document.getElementById("Usuario").value
     if(password === password2){
         
         //Pega o localStorage que já existe ou cria um novo vazio se não tiver
